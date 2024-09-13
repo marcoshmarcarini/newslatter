2f4fba6672f8f9187f71f8ac669d1941dfe14b61
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-09-13 17:15:48 -0300

    Update

---
+++ src/components/theme/Footer/page.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            Site desenvolvido por Marcos Henrique Marcarini Junior. &copy 2024, todos os direitos reservados.
+        </footer>
+    )
+}
